89d41582227aea03c3a930d16cd5439e28e3b03d
bencreizy <bencreizy@gmail.com>
2026-07-02 12:06:33 -0400

    Modify package find pattern and add Python version
    Updated package inclusion pattern and added Python version requirement.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="lexip",
     version="1.0.0",
-    packages=find_packages(),
+    packages=find_packages(include=["layer*", "lexip*"]),
     install_requires=[
         "numpy",
         "scipy",
@@ -17,4 +17,5 @@ setup(
             "lexip=layer7_extensions.lexip_cli:main",
         ],
     },
+    python_requires=">=3.9",
 )
